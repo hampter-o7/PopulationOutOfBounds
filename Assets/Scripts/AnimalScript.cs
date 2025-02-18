b587e7839b4a5a89c38284d2eed352535cad8e5b
dominikpin <dp4550@student.uni-lj.si>
2025-02-18 07:11:00 +0100

    scuffed animal movement

--- Assets/Scripts/AnimalScript.cs
+++ Assets/Scripts/AnimalScript.cs
@@ -1,12 +1,50 @@
+using System.Data;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 public class AnimalScript : MonoBehaviour
 {
-    private System.String Species;
-    private Sprite Image;
+    public GameObject animal;
+    private Sprite image;
+    public Tilemap ground;
+    public Tilemap fence;
+    Vector3 destPoint;
+    bool hasDestPoint;
+    [SerializeField] float range = 1;
+    [SerializeField] float movementSpeed;
+
+    void Start()
+    {
+        animal = GameObject.Find("Animal");
+    }
+
+    void Update()
+    {
+        AnimalMovement();
+    }
 
     public void AnimalMovement()
     {
+        if (!hasDestPoint) SearchForDest();
+        if (hasDestPoint) transform.position = Vector3.MoveTowards(transform.position, destPoint, movementSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, destPoint) < 1) hasDestPoint = false;
+    }
 
+    void SearchForDest()
+    {
+        float x = Random.Range(-range, range);
+        float y = Random.Range(-range, range);
+
+        destPoint = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+        Vector3Int groundDest = ground.WorldToCell(destPoint);
+        Vector3Int fenceDest = fence.WorldToCell(destPoint);
+        if (ground.HasTile(groundDest) &&
+        !fence.HasTile(fenceDest + Vector3Int.down) &&
+        !fence.HasTile(fenceDest + Vector3Int.down + Vector3Int.left) &&
+        !fence.HasTile(fenceDest + Vector3Int.down + Vector3Int.right))
+        {
+            hasDestPoint = true;
+        }
     }
 }
