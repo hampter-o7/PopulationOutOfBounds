8e552b63ebb10b2338e1744f8dfc7caaa3a91c50
dominikpin <dp4550@student.uni-lj.si>
2025-02-18 04:50:17 +0100

    trying out fences

---
+++ Assets/Scripts/AnimalScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AnimalScript : MonoBehaviour
+{
+    private System.String Species;
+    private Sprite Image;
+
+    public void AnimalMovement()
+    {
+
+    }
+}
