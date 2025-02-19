59f0f0eb9149bf0a954375292ca8de64713e0663
dominikpin <dp4550@student.uni-lj.si>
2025-02-19 10:54:15 +0100

    added UI start screen

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -5,13 +5,11 @@ public class AnimalSpawner : MonoBehaviour
     GameObject parent;
     [SerializeField] GameObject animal;
     //[SerializeField] AnimalSpawnerScript animalSpawnerScript;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         parent = transform.parent?.gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
